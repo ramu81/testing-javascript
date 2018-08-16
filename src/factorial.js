9f984d21bc86638343fb1a7255a2e2226571f53f
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-08-16 08:59:27 -0400

    Add files via upload

--- src/factorial.js
+++ src/factorial.js
@@ -1,9 +1,10 @@
 let factorial = function(num){
-	if (num < 0) 
-        return -1;
-	 else if (num == 0) 
+	
+	if (num < 0) throw new Error('Nagative input not expected.');    
+	
+	if (num == 0) 
       return 1;
-    else 
-        return (num * factorial(num - 1));
+    
+	return (num * factorial(num - 1));
 }
 module.exports = factorial;
