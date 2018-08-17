92db5da81fab4b943d10d5742ad67e86fd9cc437
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-08-16 23:22:31 -0400

    Add files via upload

--- src/factorial.js
+++ src/factorial.js
@@ -1,10 +1,10 @@
-let factorial = function(num) {
+let _ = require('underscore');
+
+let factorial = function(num){
 	
 	if (num < 0) throw new Error('Nagative input not expected.');    
 	
-	if (num == 0) return 1;
-    
-	return (num * factorial(num - 1));
+	return _.range(1,num +1 )
+			.reduce(function(p,e) { return p * e },1);
 }
-
 module.exports = factorial;
--- test/factorial-test.js
+++ test/factorial-test.js
@@ -3,17 +3,18 @@ let factorial = require('../src/factorial');
 
 describe('factorial test',function(){
 		
-	it('It should return 1 for input 0',function(){
-		expect(factorial(0)).to.be.eql(	1);
-	});
+	[[0,1],[1,1],[2,2],[3,6],[5,120]].forEach(function(i){
 	
-	it('It should return 6 for input 3',function(){
-		expect(factorial(3)).to.be.eql(6);
-	});	
+	it('It should return '+i[1]+' input '+i[0],function(){
+			expect(factorial(i[0])).to.be.eql(i[1]);
+		});
+		
+	});
 	
 	it('It should return Exception for input -1',function(){
 		
 		expect(function() { factorial(-1)}).to.throw("Nagative input not expected.")
 		
 	});
+	
 });
